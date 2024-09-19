b19802fc13506eb6bae579cb3afb86e9031fd8d7
rezar <rhusai2@uic.edu>
2024-09-18 20:22:20 -0500

    Added associations for Cohort

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -16,4 +16,6 @@
 class Cohort < ApplicationRecord
   belongs_to :creator, required: true, class_name: 'User', foreign_key: 'creator_id'
   belongs_to :program, required: true, class_name: 'Program', foreign_key: 'program_id'
+  has_many  :matches, class_name: "Match", foreign_key: "cohort_id", dependent: :destroy
+
 end
