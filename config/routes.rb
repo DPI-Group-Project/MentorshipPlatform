b52c875ef197644b1c6c5998d5dbbc20fb3143d1
MayaS111 <mayasheriff2018@gmail.com>
2024-10-03 23:31:39 -0500

    added controller actions to routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/dashboard/:role"
-  get "/profile/:id"
+  get '/dashboard/:role', to: 'dashboard#show'
+  get '/profile/:id', to: 'profile#show'
 
   # Defines the root path route ("/")
   root "home#index"
