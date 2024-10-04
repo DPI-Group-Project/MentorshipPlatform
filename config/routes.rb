e5c00b6f6b84cdecbae32f6aae3a5c2ef6621272
MayaS111 <mayasheriff2018@gmail.com>
2024-10-03 22:43:16 -0500

    created profile route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/dashboard/:role"
+  get "/profile/:id"
 
   # Defines the root path route ("/")
   root "home#index"
