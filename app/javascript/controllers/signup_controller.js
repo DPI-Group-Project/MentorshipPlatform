c13547e875e2afb0d74ec179ecaef3c8f9437308
brvarner <bvarner4592@gmail.com>
2024-10-05 15:24:52 -0500

    added stimulus controller to signup page

--- app/javascript/controllers/signup_controller.js
+++ app/javascript/controllers/signup_controller.js
@@ -2,7 +2,14 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="signup"
 export default class extends Controller {
+  static targets = ["display"];
   connect() {
     console.log("Hello!", this.element);
+  }
+
+  showForm({ params: { content } }) {
+    console.log(content);
+
+    const display = this.displayTarget
   }
 }
