e8b0f1017ca09a36e5f03d7b5c764680b155b87e
brvarner <bvarner4592@gmail.com>
2024-10-05 14:50:08 -0500

    added mentor and mentee forms along with signup page

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,12 @@ Rails.application.routes.draw do
   resources :programs
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'home#signup'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "home#index"
+  root 'home#index'
 end
