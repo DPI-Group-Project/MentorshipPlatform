2ea82de2704add50e37515cd7824b03c2a2fcef0
Reza Husain <rezahusain99@gmail.com>
2024-10-06 21:00:13 -0500

    Refactored dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,11 @@
 class DashboardController < ApplicationController
   before_action :dashboard_params, only: [:show]
   def show
-    @role = params[:role]
-
-    case @role
-    when 'mentor'
+    @role = params[:role].downcase
+    #Load up data when role is valid
+    if ['mentor', 'mentee', 'admin'].include? (@role) then
       @mentors_data = CohortMember.where(role: 'Mentor')
-    when 'mentee'
       @mentees_data = CohortMember.where(role: 'Mentee')
-    when 'admin'
       @admins_data = ProgramAdmin.all
     else
       redirect_to root_path, alert: 'Invalid role specified.'
