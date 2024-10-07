5056ec3973395d15695402afd94e4a1857c7d486
brvarner <bvarner4592@gmail.com>
2024-10-07 17:00:29 -0500

    set up the registration, moved files and partials to subfolders

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -10,9 +10,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
+
+  def signup
+    @user = User.new
+    render 'users/registrations/signup'
+  end
 
   # GET /resource/edit
   # def edit
@@ -41,9 +46,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[first_name last_name status inactive_reason phone_number bio timezone title linkedin_link profile_picture
+                                               skills_array])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,12 @@ Rails.application.routes.draw do
   resources :matches
   resources :cohorts
   resources :programs
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'signup', to: 'home#signup'
+  devise_scope :user do
+    get 'signup', to: 'users/registrations#signup'
+  end
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
