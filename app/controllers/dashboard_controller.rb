343ebec28a46a0ae0775f61dc714519eb50b541b
MayaS111 <mayasheriff2018@gmail.com>
2024-10-08 19:43:36 -0500

    fixing comments

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_action :dashboard_params, only: [:show]
   def show
     @role = params[:role].downcase    
-    #Load up data when role is valid
+    #Loads up data when role is valid
     if ['mentor', 'mentee', 'admin'].include? (@role) then
       @mentors_data = User.mentors_in_cohort(current_user.cohort)
       @mentees_data = CohortMember.where(role: 'Mentee')
