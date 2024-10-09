42233f2478ce82a2485ccda602e1ba6a70337f73
APatel-AI <arpan.p.se@gmail.com>
2024-10-09 12:22:27 -0500

    updated nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   resources :cohorts
   resources :programs
   devise_for :users
+
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
