fcf4649fe0fbddc7fc2889488201a1ae068d9e0f
MayaS111 <mayasheriff2018@gmail.com>
2024-10-08 19:39:28 -0500

    removed .first method on user

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,7 +4,7 @@ class ProfileController < ApplicationController
   before_action :set_capacity_info, only: [:show, :create]  
   
   def show
-    @role = @user.first.role
+    @role = @user.role
   end
 
   def create 
@@ -12,22 +12,22 @@ class ProfileController < ApplicationController
 
     respond_to do |format|
       if @current_mentee_count < @capacity_cap && current_user.matched? == false
-        match = Match.create(mentor_id: @user.first.id, mentee_id: current_user.id, cohort_id: current_user.cohort, active: true)
+        match = Match.create(mentor_id: @user.id, mentee_id: current_user.id, cohort_id: current_user.cohort, active: true)
         
         #TODO: Update mentee dashboard
         #if capacity is reached remove mentor from list of mentors on dashboard
         #if mentee is already matched dont show match button
 
         if match.save
-          format.html { redirect_to "/profile/#{@user.first.id}", notice: 'You are now matched!' }
+          format.html { redirect_to "/profile/#{@user.id}", notice: 'You are now matched!' }
           format.json { render :show, status: :created, location: @message }
           format.js
         else
-          format.html { redirect_to "/profile/#{@user.first.id}", alert: 'Error has occurred. Not matched' }
+          format.html { redirect_to "/profile/#{@user.id}", alert: 'Error has occurred. Not matched' }
           format.json { head :no_content }
         end
       else
-        format.html { redirect_to "/profile/#{@user.first.id}", alert: 'Sorry, this mentor has reached their intake capacity. Try a different mentor.' }
+        format.html { redirect_to "/profile/#{@user.id}", alert: 'Sorry, this mentor has reached their intake capacity. Try a different mentor.' }
         format.json { head :no_content }
       end
     end
@@ -35,13 +35,11 @@ class ProfileController < ApplicationController
 
   private
   def set_capacity_info
-    @current_mentee_count = @user.first.mentee_capacity_count(@user.first.cohort)
-    @capacity_cap = @user.first.capacity
+    @current_mentee_count = @user.mentee_capacity_count(@user.cohort)
+    @capacity_cap = @user.capacity
   end
   def set_profile_user
-    @user = User.where(id: params[:id])
-    # @current_user = User.mentees_in_cohort(@user.first.cohort).sample   #temporary test current_user
-    # @current_user = User.where(id: 3918).first   #temporary test current_user
+    @user = User.find_by(id: params[:id])
   end
   # Only allow a list of trusted parameters through.
   def profile_params
