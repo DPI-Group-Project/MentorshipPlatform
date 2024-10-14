d77ee567511d97c54190c83d5879b1145afbf541
MayaS111 <mayasheriff2018@gmail.com>
2024-10-14 18:26:19 -0500

    selecting a program works and cohorts of that program renders

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,7 +10,10 @@ class DashboardController < ApplicationController
       @admin_data = ProgramAdmin.find_by(id: current_user.id)
       @programs_by_admin = Program.where(creator_id: current_user.id)
       if params[:program_id].present?
-        @current_program = Program.find(params[:program_id])
+        @current_program = Program.find_by(id: params[:program_id])
+        @cohorts = Cohort.where(program_id: params[:program_id])
+      else
+        @current_program = Program.where(creator_id: current_user.id).sample
         @cohorts = Cohort.where(program_id: @current_program.id)
       end
     else
