1e318ccee61e376c9de945fceffb168f9269fd74
brvarner <bvarner4592@gmail.com>
2024-10-23 11:32:06 -0500

    moved find_or_create callback into cohort_member_controller, users now being built based on id and given a random pw

--- app/controllers/cohort_members_controller.rb
+++ app/controllers/cohort_members_controller.rb
@@ -1,5 +1,6 @@
 class CohortMembersController < ApplicationController
-  before_action :set_cohort_member, only: %i[ show edit update destroy ]
+  before_action :set_cohort_member, only: %i[show edit update destroy]
+  before_action -> { find_or_create_user(cohort_member_params) }, only: [:create]
 
   # GET /cohort_members or /cohort_members.json
   def index
@@ -25,7 +26,7 @@ class CohortMembersController < ApplicationController
 
     respond_to do |format|
       if @cohort_member.save
-        format.html { redirect_to cohort_member_url(@cohort_member), notice: "Cohort member was successfully created." }
+        format.html { redirect_to cohort_member_url(@cohort_member), notice: 'Cohort member was successfully created.' }
         format.json { render :show, status: :created, location: @cohort_member }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +39,7 @@ class CohortMembersController < ApplicationController
   def update
     respond_to do |format|
       if @cohort_member.update(cohort_member_params)
-        format.html { redirect_to cohort_member_url(@cohort_member), notice: "Cohort member was successfully updated." }
+        format.html { redirect_to cohort_member_url(@cohort_member), notice: 'Cohort member was successfully updated.' }
         format.json { render :show, status: :ok, location: @cohort_member }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +53,29 @@ class CohortMembersController < ApplicationController
     @cohort_member.destroy!
 
     respond_to do |format|
-      format.html { redirect_to cohort_members_url, notice: "Cohort member was successfully destroyed." }
+      format.html { redirect_to cohort_members_url, notice: 'Cohort member was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cohort_member
-      @cohort_member = CohortMember.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def cohort_member_params
-      params.require(:cohort_member).permit(:email, :cohort_id, :role)
+  # Use callbacks to share common setup or constraints between actions.
+  def set_cohort_member
+    @cohort_member = CohortMember.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def cohort_member_params
+    params.require(:cohort_member).permit(:email, :cohort_id, :role)
+  end
+
+  def find_or_create_user(cohort_member_params)
+    user = User.find_or_create_by(email: cohort_member_params[:email]) do |u|
+      u.password = SecureRandom.base36(10)
     end
+    return unless user.new_record?
+
+    user.save!
+  end
 end
--- app/models/cohort_member.rb
+++ app/models/cohort_member.rb
@@ -13,13 +13,4 @@
 class CohortMember < ApplicationRecord
   belongs_to :user, required: true, class_name: 'User', primary_key: 'email', foreign_key: 'email'
   belongs_to :cohort, required: true, class_name: 'Cohort', foreign_key: 'cohort_id'
-
-  before_create :find_or_create_user
-  attr_accessor :cohort_member_attributes
-
-  private
-
-  def find_or_create_user
-    pp cohort_member_attributes
-  end
 end
