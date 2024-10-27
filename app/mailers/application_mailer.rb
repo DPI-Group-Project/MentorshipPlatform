fde21f005eb56955f452a48616f6d971f1c038ee
brvarner <bvarner4592@gmail.com>
2024-10-27 18:10:49 -0500

    added mail layouts

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'info@mente.com'
+  layout 'mailer'
 end
--- app/mailers/cohort_member_mailer.rb
+++ app/mailers/cohort_member_mailer.rb
@@ -1,2 +1,18 @@
 class CohortMemberMailer < ApplicationMailer
+  def welcome_signup
+    @user = params[:user]
+    @role = params[:role]
+    @cohort = params[:cohort]
+    if @role == 'mentee'
+      mail(to: email_address_with_name(@user.email, @user.first_name)) do |format|
+        format.html { render layout: 'mentee_welcome_mail' }
+        format.text
+      end
+    elsif @role == 'mentor'
+      mail(to: email_address_with_name(@user.email, @user.first_name)) do |format|
+        format.html { render layout: 'mentor_welcome_mail' }
+        format.text
+      end
+    end
+  end
 end
