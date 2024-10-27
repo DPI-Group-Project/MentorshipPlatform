5fac6627ff115de3e2f5358a340d9ea4f6adecb3
brvarner <bvarner4592@gmail.com>
2024-10-27 14:52:48 -0500

    generated cohort member mailer

---
+++ app/mailers/cohort_member_mailer.rb
@@ -0,0 +1,2 @@
+class CohortMemberMailer < ApplicationMailer
+end
---
+++ test/mailers/cohort_member_mailer_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CohortMemberMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
