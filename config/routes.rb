eb70ada3198e789e003472be849500eceec9703b
APatel-AI <arpan.p.se@gmail.com>
2024-10-27 18:02:25 -0500

    created working feature for mentor and mentee adding

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   resources :meetings
-  resources :cohort_members
+  resources :cohort_members do
+    # Collection routes for adding and deleting emails without a specific cohort_member ID
+    post :add_email, on: :collection, as: :add_email
+    post :delete_email, on: :collection, as: :delete_email
+  end
   resources :organizations
   resources :reviews
   resources :matches
   resources :cohorts
   resources :programs
   devise_for :users, controllers: { registrations: 'users/registrations' }
-
+ 
 
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
