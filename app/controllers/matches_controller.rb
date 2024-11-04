e1085d5ad15b267430fe37f13f3a92320eedf3f1
young <hyangyoungs@gmail.com>
2024-11-04 12:31:03 -0600

    feat: matching algorithm start at POST matches/new

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -19,20 +19,68 @@ class MatchesController < ApplicationController
   def edit
   end
 
-  # POST /matches or /matches.json
   def create
-    @match = Match.new(match_params)
+    cohort_id = 1  # TODO: get cohort_id from param
+    sorted_shortlist = ShortList.where(cohort_id: cohort_id)
+                                .order(:ranking, :created_at)
 
-    respond_to do |format|
-      if @match.save
-        format.html { redirect_to match_url(@match), notice: 'Match was successfully created.' }
-        format.json { render :show, status: :created, location: @match }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @match.errors, status: :unprocessable_entity }
-      end
+    sorted_shortlist.each do |shortlist|
+      mentor = User.find(shortlist.mentor_id)
+      mentee = User.find(shortlist.mentee_id)
+      cohort = Cohort.find(shortlist.cohort_id)
+
+      next if Match.exists?(mentee_id: mentee.id, cohort_id: cohort.id)
+      cohort_member = CohortMember.find_by(email: mentor.email, cohort_id: cohort.id)
+      next if cohort_member.nil? || cohort_member.capacity <= 0
+
+      Match.create!(
+        mentor_id: mentor.id,
+        mentee_id: mentee.id,
+        cohort_id: cohort.id,
+        active: true
+      )
+      cohort_member.update!(capacity: cohort_member.capacity - 1)
     end
+
+    flash[:notice] = "Matches created successfully."
+    redirect_to matches_path
+
+  rescue ActiveRecord::RecordInvalid => e
+    flash[:alert] = "Failed to create matches: #{e.message}"
+    redirect_to matches_path
   end
+
+  # POST /matches or /matches.json
+  # def create
+  #   cohort_id = 1
+  #   mentors = CohortMember.where(role: 'mentor', cohort_id: cohort_id).select(:email, :capacity)
+  #
+  #   mentors.each do |mentor|
+  #     user = User.find_by(email: mentor.email)
+  #     next unless user
+  #
+  #     shortlisted_mentees = ShortList.where(mentor_id: user.id)
+  #                                    .order(:ranking, :created_at)
+  #                                    .limit(mentor.capacity)
+  #
+  #     shortlisted_mentees.each do |shortlist|
+  #       Match.create!(
+  #         mentor_id: user.id,
+  #         mentee_id: shortlist.mentee_id,
+  #         cohort_id: shortlist.cohort_id,
+  #         active: true
+  #       )
+  #     end
+  #
+  #     mentor.update(capacity: mentor.capacity - shortlisted_mentees.count)
+  #   end
+
+  #   flash[:notice] = "Matches created successfully."
+  #   redirect_to matches_path
+  # rescue ActiveRecord::RecordInvalid => e
+  #   flash[:alert] = "Failed to create matches: #{e.message}"
+  #   redirect_to matches_path
+  # end
 
   # PATCH/PUT /matches/1 or /matches/1.json
   def update
