cf3628087d452c03be1522afc9bbf1bcb2b5848d
brvarner <bvarner4592@gmail.com>
2024-11-05 13:15:32 -0600

    Added form partial to program admin views

--- app/models/program_admin.rb
+++ app/models/program_admin.rb
@@ -11,5 +11,4 @@
 class ProgramAdmin < ApplicationRecord
   belongs_to :program, required: true, class_name: "Program", foreign_key: "program_id"
   belongs_to :admin, required: true, class_name: "User", foreign_key: "user_id"
-
 end
