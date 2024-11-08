7a680bdae706a55e4ca3985b11b82312a0faac1a
MayaS111 <maya@tenacity828.com>
2024-11-07 22:23:24 -0600

    COMMENTS FOR TESTING

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -9,6 +9,7 @@ cohorts.each_with_index do |cohort, index|
   shortlist_end_date_[index] = cohort.shortlist_end_date
   scheduler.at "#{shortlist_end_date_[index]}" do
     # Call your task (the Matching logic)
+    p "ITS TIME TO MATCH 1"
     Match.run_matching(cohort.id)
   end
 end
@@ -17,6 +18,7 @@ user_timezone = 'America/Chicago'
 current_time = Time.current.in_time_zone(user_timezone)
 current_time_in_user_zone = current_time.strftime('%Y-%m-%d %H:%M:%S UTC')
 if current_time_in_user_zone == "2024-11-07 22:15:00"
+  p "ITS TIME TO MATCH 2"
   scheduler.at "#{current_time}" do
     # Call your task (the Matching logic)
     Match.run_matching(502)
