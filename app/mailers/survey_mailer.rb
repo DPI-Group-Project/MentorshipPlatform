87caf6ee20cce11d11166092924ad5cc819e97a8
borvux <bennyjoram@gmail.com>
2024-11-11 18:56:27 -0600

    created survey notification

--- app/mailers/survey_mailer.rb
+++ app/mailers/survey_mailer.rb
@@ -1,2 +1,10 @@
 class SurveyMailer < ApplicationMailer
+	def survey_created_notification(admin, survey)
+		@survey = survey
+		@admin = admin
+		mail(
+			to: @admin.email,
+			subject: "New Survey Created for #{survey.program.name}"
+		)
+	end
 end
