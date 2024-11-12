f9ece0dd3e23d6bb712296f6f0692d6ceb0df457
borvux <bennyjoram@gmail.com>
2024-11-11 18:41:22 -0600

    created matched func in the match mailer to send to mentor and mentee when they are matched

--- app/mailers/match_mailer.rb
+++ app/mailers/match_mailer.rb
@@ -1,2 +1,7 @@
 class MatchMailer < ApplicationMailer
+	def match_created(match)
+    @match = match
+    mail(to: @match.mentor.email, subject: 'New Match Created')
+    mail(to: @match.mentee.email, subject: 'New Match Created')
+  end
 end
