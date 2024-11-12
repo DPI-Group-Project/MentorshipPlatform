4aff8c358968b082a7db19c6b3e78eff3b8874e2
MayaS111 <mayasheriff20@gmail.com>
2024-11-12 01:06:44 -0600

    added validations, clean up code, refactoring, "emails" get sent (must get mailer to send actual emails out), matching reopens (does 6 days instead of 3 MUST FIX)

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -8,14 +8,39 @@ Rails.application.config.to_prepare do
     require 'rufus-scheduler'
     # Initialize a new scheduler instance
     scheduler = Rufus::Scheduler.new
-    cohorts = Cohort.where.not(shortlist_start_time: nil)
+    cohorts = Cohort.where.not(start_date: nil)
+          .where.not(end_date: nil)
+          .where.not(shortlist_end_time: nil)
           .where.not(shortlist_end_time: nil)
 
-    cohorts.each_with_index do |cohort, index|
+    cohorts.each do |cohort|
       shortlist_end_date = cohort.shortlist_end_time.in_time_zone.utc
+
+      # trigger matching when the scheduler is reached
       scheduler.at shortlist_end_date do
         MatchesController.new.create_for_cohort(cohort)
       end
+    
+      # Schedule the email notification at the shortlist start time
+      scheduler.at cohort.shortlist_start_time.in_time_zone.utc do
+        cohort.members.each do |member|
+          CohortMailer.shortlist_start_notification(member.user, cohort).deliver_later
+        end
+      end
+
+      # send warning email to admin that cohort is ending in 2 weeks
+      scheduler.at cohort.end_date - 2.weeks do
+        CohortMailer.two_week_warning(creator.email, cohort).deliver_later
+
+        cohort_members = CohortMember.where(cohort_id: cohort.id)
+        # remind each cohort member about survey
+        cohort_members.each do |member|
+          CohortMailer.survey_reminder(member.mentor, cohort).deliver_later
+          CohortMailer.survey_reminder(member.mentee, cohort).deliver_later
+        end
+
+        CohortMailer.survey_reminder(nil, cohort, cohort.creator.email).deliver_later
+      end
     end
-  end 
+  end
 end
