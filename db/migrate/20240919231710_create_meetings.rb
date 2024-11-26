31e66a78c58e825c3ee7fbe9b320246fdb1710c7
SaraDawner2000 <83845798+SaraDawner2000@users.noreply.github.com>
2024-11-26 13:25:02 -0600

    finish rolling up meetings

--- db/migrate/20240919231710_create_meetings.rb
+++ db/migrate/20240919231710_create_meetings.rb
@@ -2,11 +2,11 @@ class CreateMeetings < ActiveRecord::Migration[7.1]
   def change
     create_table :meetings do |t|
       t.references :match, null: false, foreign_key: true
-      t.boolean :complete
       t.date :date
       t.time :time
       t.text :notes
       t.string :location
+      t.string :location_type
 
       t.timestamps
     end
