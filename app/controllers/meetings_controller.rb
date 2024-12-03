29e5e239ffdd5d8b1fbb7bcde1bd970227b2663d
Reza Husain <rezahusain99@gmail.com>
2024-12-03 00:06:58 -0600

    Update meeting table for each link

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -3,9 +3,17 @@ class MeetingsController < ApplicationController
 
   # GET /meetings or /meetings.json
   def index
-    @match = Match.where("mentee_id = :id OR mentor_id = :id", id: current_user.id).first
-    @meetings = @match.meetings.order(:date)
+    if params[:mentor_id]
+      @match = Match.find_by(mentor_id: params[:mentor_id], mentee_id: current_user.id)
+    elsif params[:mentee_id]
+      @match = Match.find_by(mentee_id: params[:mentee_id], mentor_id: current_user.id)
+    else
+      flash[:alert] = "No match found."
+      redirect_to dashboard_path(role: current_user.role) and return
+    end
+    @meetings = @match.meetings.order(:date) if @match
     @mentor = @match.mentor
+    @mentee = @match.mentee
     @required_meetings_count = @match.cohort.required_meetings
     @remaining_meetings = @required_meetings_count - @meetings.count
   end
@@ -21,6 +29,16 @@ class MeetingsController < ApplicationController
 
   # GET /meetings/new
   def new
+    if params[:mentee_id]
+      @mentee = User.find(params[:mentee_id])
+      @match = Match.find_by(mentee_id: @mentee.id, mentor_id: current_user.id)
+    elsif params[:mentor_id]
+      @mentor = User.find(params[:mentor_id])
+      @match = Match.find_by(mentor_id: @mentor.id, mentee_id: current_user.id)
+    else
+      flash[:alert] = "No valid match found."
+      redirect_to dashboard_path(role: current_user.role) and return
+    end
     @meeting = Meeting.new
   end
 
@@ -29,7 +47,21 @@ class MeetingsController < ApplicationController
 
   # POST /meetings or /meetings.json
   def create
-    @match = Match.find_by(mentee_id: current_user.id)
+    # Use mentee_id or mentor_id passed as parameters to find the match
+    if params[:mentee_id].present?
+      @match = Match.find_by(mentee_id: params[:mentee_id], mentor_id: current_user.id)
+    elsif params[:mentor_id].present?
+      @match = Match.find_by(mentor_id: params[:mentor_id], mentee_id: current_user.id)
+    else
+      flash[:alert] = "No valid match found for the given user."
+      redirect_to dashboard_path(role: current_user.role) and return
+    end
+
+    # Ensure a match is found
+    if @match.nil?
+      flash[:alert] = "No match found for the provided user."
+      redirect_to dashboard_path(role: current_user.role) and return
+    end
     @meeting = Meeting.new(meeting_params)
     @meeting.match_id = @match.id if @match.present?
 
