86a1a38a29abac49c6fccc88fc19c01046770f51
brvarner <bvarner4592@gmail.com>
2024-12-04 14:54:50 -0600

    added previews for emails

--- test/mailers/previews/match_mailer_preview.rb
+++ test/mailers/previews/match_mailer_preview.rb
@@ -1,3 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/match_mailer
 class MatchMailerPreview < ActionMailer::Preview
+  def match_created
+    match = Match.first
+    MatchMailer.with(match: match).match_created
+  end
 end
