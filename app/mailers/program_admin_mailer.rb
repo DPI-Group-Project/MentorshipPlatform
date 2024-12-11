648758d9903981a191782737b4096d03f0a19625
borvux <bennyjoram@gmail.com>
2024-12-10 20:34:39 -0600

    created admin_created_email method to send mail after new admin is added

--- app/mailers/program_admin_mailer.rb
+++ app/mailers/program_admin_mailer.rb
@@ -1,2 +1,6 @@
 class ProgramAdminMailer < ApplicationMailer
+	def admin_created_email(admin_email, program_name)
+    @program_name = program_name
+    mail(to: admin_email, subject: 'MentE: You have been added as an Admin')
+  end
 end
