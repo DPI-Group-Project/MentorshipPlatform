9bcb1f2cbf491132ae17120992145955890880f6
SaraDawner2000 <83845798+SaraDawner2000@users.noreply.github.com>
2024-12-17 20:06:46 -0600

    add composite uniqueness constraint on short_lists

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,8 +1,7 @@
 class MatchesController < ApplicationController
-  before_action :set_match, only: %i[show edit update destroy]
+  before_action :set_match, only: %i[update destroy]
   before_action :set_cohort, only: %i[index create update]
 
-  # GET /matches or /matches.json
   def index
     @cohort = Cohort.find_by(id: @cohort_id)
     @matches = Match.where(cohort_id: @cohort_id)
@@ -12,46 +11,27 @@ class MatchesController < ApplicationController
     @match = Match.new
   end
 
-  # GET /matches/1 or /matches/1.json
-  def show; end
-
   def new
     @match = Match.new
     @cohort = Cohort.find(params[:cohort_id])
   end
 
-  # GET /matches/1/edit
-  def edit; end
-
   def create
     @match = Match.new(match_params)
 
     if @match.save
-      # Respond with JS (AJAX)
       respond_to do |format|
         format.html { redirect_to matches_path(cohort_id: @cohort_id), notice: "Match created successfully!" }
-        format.js # This will call create.js.erb
+        format.js
       end
     else
-      # If validation fails, respond with JS to show errors
       respond_to do |format|
         format.html { redirect_to matches_path(cohort_id: @cohort_id), alert: "Match was not created, try again" }
-        format.js # This will trigger create.js.erb to handle errors
+        format.js
       end
     end
   end
 
-  # def create
-  #   create_matches_for_cohort(@cohort_id)
-
-  #   flash[:notice] = "Matches created successfully."
-  #   redirect_to matches_path
-  # rescue ActiveRecord::RecordInvalid => e
-  #   flash[:alert] = "Failed to create matches: #{e.message}"
-  #   redirect_to matches_path
-  # end
-
-  # PATCH/PUT /matches/1 or /matches/1.json
   def update
     respond_to do |format|
       if @match.update(match_params)
@@ -64,7 +44,6 @@ class MatchesController < ApplicationController
     end
   end
 
-  # DELETE /matches/1 or /matches/1.json
   def destroy
     @match.destroy!
 
@@ -81,7 +60,6 @@ class MatchesController < ApplicationController
     CohortMatchingService.new(cohort).call
   end
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_match
     @match = Match.find(params[:id])
   end
@@ -90,7 +68,6 @@ class MatchesController < ApplicationController
     @cohort_id = params[:cohort_id]
   end
 
-  # Only allow a list of trusted parameters through.
   def match_params
     params.require(:match).permit(:mentor_id, :mentee_id, :cohort_id, :active)
   end
