1e3bcf86a176a35f2625bb5678a3d6a7bb3afc88
brvarner <bvarner4592@gmail.com>
2025-01-07 11:18:48 -0600

    updated cohort member migration to use email index as main index

--- db/migrate/20240919225754_create_cohort_members.rb
+++ db/migrate/20240919225754_create_cohort_members.rb
@@ -2,14 +2,14 @@ class CreateCohortMembers < ActiveRecord::Migration[7.1]
   def change
     create_enum :role, %w[mentor mentee]
 
-    create_table :cohort_members, id: false, primary_key: :email do |t|
+    create_table :cohort_members, id: false do |t|
+      t.string :email, null: false, primary_key: true
       t.references :cohort, null: false, foreign_key: true
-      t.string :email, null: false
       t.enum :role, enum_type: :role
       t.integer :capacity
       t.timestamps
 
-      t.index ["email"], unique: true
     end
+    add_index :cohort_members, :email, unique: true
   end
 end
