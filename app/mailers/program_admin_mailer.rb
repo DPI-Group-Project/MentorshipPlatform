f20540b1c942292ea0069430b2e42d64e50aac06
brvarner <bvarner4592@gmail.com>
2025-01-30 11:12:56 -0600

    updated the admin email params and the mailer view

--- app/mailers/program_admin_mailer.rb
+++ app/mailers/program_admin_mailer.rb
@@ -1,6 +1,8 @@
 class ProgramAdminMailer < ApplicationMailer
-  def admin_created_email(admin_email, program_name)
-    @program_name = program_name
+  def admin_created_email
+    @current_program = params[:current_program]
+    @admin = params[:program_admin]
+    @password = params[:password]
     mail(to: admin_email, subject: 'MentE: You have been added as an Admin')
   end
 end
